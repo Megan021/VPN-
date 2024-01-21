4eefbb97a231ea0d8efbc0836c66fe1c3e290718
Megan021 <megan.nagarkoti123@gmail.com>
2024-01-21 13:01:36 +0545

    changes made on app.css, app.jsx and index.css to merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import React from 'react'
 import './App.css'
 import Contact from './components/Contact'
@@ -7,32 +6,23 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import News from './components/News'
 import Subscribe from './components/Subscribe'
-=======
-import { useState } from 'react'
-import './App.css'
 import About from './components/About'
 import Maping from './components/Maping'
 import Team from './components/Team'
->>>>>>> origin/Dipendra
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
     <Navbar />
     <Hero />
     <News />
     <Contact />
     <Subscribe />
     <Footer />
-=======
-      <div>
         <About/>
         <Maping/>
         <Team/>
-      </div>
->>>>>>> origin/Dipendra
     </>
   )
 }
