61cb7297780f5818239040ddd7666bbda8f695ba
Megan021 <megan.nagarkoti123@gmail.com>
2024-01-21 12:08:34 +0545

    changes to contact page and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import './App.css'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
@@ -13,6 +14,7 @@ function App() {
     <Navbar />
     <Hero />
     <News />
+    <Contact />
     <Subscribe />
     <Footer />
     </>
